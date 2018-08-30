ac5fc24f6eefc8d7c1a299db1f13f4299f7b135a
Pearl Law <pearllynnelaw@gmail.com>
2018-08-30 10:59:28 -0700

    Define a function that updates the elapsed seconds and writes it to the DOM. #1

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ var $button = document.querySelector('.start')
 $button.addEventListener('click', function () {
   console.log('Hello')
 }, false)
+
+function updateTime() {
+  var $currentTime = document.querySelector('.timeElapsed')
+  var timeElapsed = parseInt($currentTime.textContent, 10)
+  timeElapsed += 1
+  $currentTime.textContent = timeElapsed
+}
