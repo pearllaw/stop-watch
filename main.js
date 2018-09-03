a2915555393e4ee3a1dadf4b461d5814a0f43d80
Pearl Law <pearllynnelaw@gmail.com>
2018-09-03 10:27:07 -0700

    Issue #4  Update tick function to conditionally stop the timer. incomplete

--- main.js
+++ main.js
@@ -1,34 +1,41 @@
 var $button = document.querySelector('.start')
 var $currentTime = document.querySelector('.timeElapsed')
 var timeElapsed = parseInt($currentTime.textContent, 10)
-var stop = null
+var time = null
+var $input = document.querySelector('#limit')
 
 //Start and pause button features
 $button.addEventListener('click', function () {
-  if (stop === null) {
-    stop = setInterval(updateTime, 1000)
+  if (time === null) {
+    time = setInterval(updateTime, 1000)
     $button.textContent = 'Pause'
     $button.classList.replace('start', 'start-clicked')
     $reset.classList.toggle('reset-visible')
   }
   else {
-    clearInterval(stop)
-    stop = null
+    clearInterval(time)
+    time = null
     $button.textContent = 'Start'
     $button.classList.replace('start-clicked', 'start')
   }
 })
 
 function updateTime() {
+  if ($input.value >= 0) {
+    $currentTime.textContent = $input.value
+    $input.value -= 1
+  }
+  else {
   timeElapsed += 1
   $currentTime.textContent = timeElapsed
+  }
 }
 
 //Reset button features
 var $reset = document.querySelector('.reset')
 
 $reset.addEventListener('click', function () {
-  clearInterval(stop)
+  clearInterval(time)
   $currentTime.textContent = 0
   $reset.classList.replace('reset-visible', 'reset')
   $button.classList.replace('start-clicked', 'start')
